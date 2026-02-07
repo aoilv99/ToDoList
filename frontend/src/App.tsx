6bff078250199c0f641820456ffd78bc02469a4d
aoilv99 <aoiiiiilv999@gmail.com>
2026-02-07 16:28:07 +0900

    タイトル画面できたあ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Title from './pages/title';
 
-function App() {
-  
-  return (
-    <>
-    
-    </>
-  )
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Title />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Title.tsx
+++ frontend/src/pages/Title.tsx
@@ -1,10 +1,27 @@
-import 'title.css'
+import Logo from '../assets/TODOLISTロゴ.png';
+import LogoImg from '../assets/マカロン.png';
 
-function Title(){
-    return
-    <>
-    
-    </>
+function Title() {
+  return (
+    <div className="w-full h-screen bg-pink-200 flex flex-col items-center justify-center">
+      
+      <div className="relative flex flex-col items-center">
+        {/* TO DO LISTロゴ */}
+        <img
+          src={Logo}
+          alt="TO DO LIST"
+          className="z-10"
+        />
+        
+        {/* マカロン画像（ロゴに重なるように配置） */}
+        <img
+          src={LogoImg}
+          alt="マカロン"
+          className="-mt-20 z-0 mr-7"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Title;
